1a1c348e0381375c8980f35ba98c3f03be60b157
codeabuu <abdulngui718@gmail.com>
2024-06-06 17:34:26 -0400

    scraping done

--- Backend/app.py
+++ Backend/app.py
@@ -12,6 +12,43 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
+class ProductResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    img = db.Column(db.String(1000))
+    url = db.Column(db.String(1000))
+    price = db.Column(db.Float)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    search_text = db.Column(db.String(255))
+    source = db.Column(db.String(255))
+
+    def __init__(self, name, img, url, price, search_text, source):
+        self.name = name
+        self.url = url
+        self.img = img
+        self.price = price
+        self.search_text = search_text
+        self.source = source
+
+
+class TrackedProducts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    tracked = db.Column(db.Boolean, default=True)
+
+    def __init__(self, name, tracked=True):
+        self.name = name
+        self.tracked = tracked
+
+
+@app.route('/results', methods=['POST'])
+def submit_results():
+    results = request.json.get(data)
+    search_text = request.json.get("search_text")
+    source = request.json.get("source")
+
+    
 
 if __name__ == "__main__":
     with app.app_context():
