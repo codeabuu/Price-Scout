2505171043550328ff536f69bce4bd864edf9513
codeabuu <abdulngui718@gmail.com>
2024-06-09 16:39:51 -0400

    components

---
+++ frontend/src/components/PriceHistoryTable.jsx
@@ -0,0 +1,4 @@
+import React, {useState} from "react";
+import ModalComponent from "./Modal";
+import ProductDetailsPage from "./ProductDetailsPage";
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Modal from 'react-Modal';
+import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 
